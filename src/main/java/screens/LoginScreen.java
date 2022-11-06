d624258a5ec3f007a2fc80e8ab6008bbfbe9104e
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-05 22:46:37 -0400

    init repo

--- src/main/java/screens/LoginScreen.java
+++ src/main/java/screens/LoginScreen.java
@@ -1,19 +1,59 @@
 package screens;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 
 public class LoginScreen {
+    JRadioButton isParticipant = new JRadioButton();
+    JRadioButton isOrganization = new JRadioButton();
+    JTextField username = new JTextField(15);
+    /**
+     * The password
+     */
+    JPasswordField password = new JPasswordField(15);
+    public LoginScreen() {
+
+        JLabel title = new JLabel("Login Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel usernameInfo = new LabelTextPanel(
+                new JLabel("Username"), username);
+        LabelTextPanel passwordInfo = new LabelTextPanel(
+                new JLabel("Password"), password);
+
+        JButton logIn = new JButton("Log in");
+        JButton cancel = new JButton("Cancel");
+
+        JPanel buttons = new JPanel();
+        buttons.add(logIn);
+        buttons.add(cancel);
+
+        logIn.addActionListener(this);
+        cancel.addActionListener(this);
+
+        JPanel main = new JPanel();
+        main.setLayout(new BoxLayout(main, BoxLayout.Y_AXIS));
+
+        main.add(title);
+        main.add(usernameInfo);
+        main.add(passwordInfo);
+        main.add(buttons);
+        this.setContentPane(main);
+
+        this.pack();
+    }
     public void actionPerformed(ActionEvent evt) {
         try {
             userLoginController.create(
-                    isParticipant.getBoolean();
-                    isOrganization.getBoolean();
+                    isParticipant.getBoolean(),
+                    isOrganization.getBoolean(),
                     username.getText(),
-                    String.valueOf(password.getPassword()),;
+                    String.valueOf(password.getPassword()));
             JOptionPane.showMessageDialog(this, "%s created.".formatted(username.getText()));
         } catch (Exception e) {
             JOptionPane.showMessageDialog(this, e.getMessage());
         }
     }
+
 }
