3b2ca020223fa3dbdc80c4db3a36e7f8b8e0211e
BenCheng2 <bencheng0609@gmail.com>
2022-11-09 17:09:50 -0500

    Version 0.9

--- src/main/java/database/EventDsGateway.java
+++ src/main/java/database/EventDsGateway.java
@@ -19,4 +19,6 @@ public interface EventDsGateway {
 
     //Should Delete All relationships with events and the event itself
     public void deleteEvent(String event_title);
+
+    public ArrayList<String> getPaticipants(String title);
 }
