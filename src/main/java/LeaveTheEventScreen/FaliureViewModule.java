93b63efabdf22a8018a46062091b10bb81a404cc
songguangyu <songguangyu.song@mail.utoronto.ca>
2022-11-09 21:10:38 -0500

    leave

---
+++ src/main/java/LeaveTheEventScreen/FaliureViewModule.java
@@ -0,0 +1,16 @@
+package LeaveTheEventScreen;
+
+import javax.swing.*;
+
+public class FaliureViewModule {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame(" ");
+        jf.setBounds(500, 250, 400, 300);
+
+        JLabel jLabel = new JLabel("You did not succeed in leaving this event", SwingConstants.CENTER);
+        jf.add(jLabel);
+
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
---
+++ src/main/java/participant_leave_the_event/EventLeaveResponseModule.java
@@ -0,0 +1,4 @@
+package participant_leave_the_event;
+
+public class EventLeaveResponseModule {
+}
