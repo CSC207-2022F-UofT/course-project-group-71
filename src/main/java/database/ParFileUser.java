b4d41465ad176376ad2417424527216b64ba2f6a
BenCheng2 <bencheng0609@gmail.com>
2022-11-12 15:14:14 -0500

    Version 1.1 Merging mistake, reopened

--- src/main/java/database/ParFileUser.java
+++ src/main/java/database/ParFileUser.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class ParFileUser implements ParDsGateway {
     public static void main(String[] args) {
         ParFileUser a =new ParFileUser();
-        a.addNotification("chengben","sss");
+        a.addNotification("sss","111");
     }
 
     public void utilStorePar(String username, String password){
