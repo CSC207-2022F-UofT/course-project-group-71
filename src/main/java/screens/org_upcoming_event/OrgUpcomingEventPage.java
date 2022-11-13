5946f852af8e4692e779202a71086577ff1841f6
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-12 21:45:00 -0500

    changes

--- src/main/java/screens/org_upcoming_event/OrgUpcomingEventPage.java
+++ src/main/java/screens/org_upcoming_event/OrgUpcomingEventPage.java
@@ -41,60 +41,67 @@ public class OrgUpcomingEventPage extends JFrame {
 
         int numberOfEvent = upcomingEvents.size();
 
-        events.setLayout(new GridLayout(numberOfEvent,0,10,10));
+        if (numberOfEvent != 0) {
 
-        int x = 0;
-        int y = 0;
+            events.setLayout(new GridLayout(numberOfEvent, 0, 10, 10));
 
-        for (String upcomingEventTitle : upcomingEvents) {
+            int x = 0;
+            int y = 0;
 
-            JButton eventTitle = new JButton(upcomingEventTitle);
-            eventTitle.addActionListener(new OrgUpcomingEventActionListener(this));
-            eventTitle.setBounds (x, y, 250, 30);
-            eventTitle.setVisible(true);
+            for (String upcomingEventTitle : upcomingEvents) {
 
-            ArrayList<Integer> times = eventDsGateway.getTime(upcomingEventTitle);
-            String time = times.get(0) + " " + times.get(1) + "-" + times.get(2) + " " +
-                    times.get(3) + ":" + times.get(4);
+                JButton eventTitle = new JButton(upcomingEventTitle);
+                eventTitle.addActionListener(new OrgUpcomingEventActionListener(this));
+                eventTitle.setBounds(x, y, 250, 30);
+                eventTitle.setVisible(true);
 
-            JLabel eventTime = new JLabel(time);
-            eventTime.setBounds (x+20, y+40, 250, 30);
-            eventTime.setVisible(true);
+                ArrayList<Integer> times = eventDsGateway.getTime(upcomingEventTitle);
+                String time = times.get(0) + " " + times.get(1) + "-" + times.get(2) + " " +
+                        times.get(3) + ":" + times.get(4);
 
-            String location = eventDsGateway.getLocation(upcomingEventTitle);
-            JLabel eventLocation = new JLabel(location);
-            eventLocation.setBounds (x+20, y+70, 250, 30);
-            eventLocation.setVisible(true);
+                JLabel eventTime = new JLabel(time);
+                eventTime.setBounds(x + 20, y + 40, 250, 30);
+                eventTime.setVisible(true);
 
-            JButton notify = new JButton("Notify");
-            notify.setActionCommand(upcomingEventTitle + "Notify");
-            notify.addActionListener(new OrgUpcomingEventActionListener(this));
-            notify.setBounds (x+250, y+15, 100, 30);
-            notify.setVisible(true);
+                String location = eventDsGateway.getLocation(upcomingEventTitle);
+                JLabel eventLocation = new JLabel(location);
+                eventLocation.setBounds(x + 20, y + 70, 250, 30);
+                eventLocation.setVisible(true);
 
-            JButton delete = new JButton("Delete");
-            delete.setActionCommand(upcomingEventTitle + "Delete");
-            delete.addActionListener(new OrgUpcomingEventActionListener(this));
-            delete.setBounds (x+250, y+55, 100, 30);
-            delete.setVisible(true);
+                JButton notify = new JButton("Notify");
+                notify.setActionCommand(upcomingEventTitle + "Notify");
+                notify.addActionListener(new OrgUpcomingEventActionListener(this));
+                notify.setBounds(x + 250, y + 15, 100, 30);
+                notify.setVisible(true);
 
-            events.add(eventTitle);
-            events.add(eventTime);
-            events.add(eventLocation);
-            events.add(notify);
-            events.add(delete);
-            y += 100;
+                JButton delete = new JButton("Delete");
+                delete.setActionCommand(upcomingEventTitle + "Delete");
+                delete.addActionListener(new OrgUpcomingEventActionListener(this));
+                delete.setBounds(x + 250, y + 55, 100, 30);
+                delete.setVisible(true);
+
+                events.add(eventTitle);
+                events.add(eventTime);
+                events.add(eventLocation);
+                events.add(notify);
+                events.add(delete);
+                y += 100;
+            }
+
+            JScrollPane eventScroll = new JScrollPane(events);
+            eventScroll.setBounds(150, 100, getConstantX() - 170, getConstantY() - 150);
+            eventScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+            eventScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+            eventScroll.setVisible(true);
+            this.add(eventScroll);
         }
 
-        JScrollPane eventScroll = new JScrollPane(events);
-        eventScroll.setBounds(150, 100, getConstantX()-170,getConstantY()-150);
-        eventScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-        eventScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-        eventScroll.setVisible(true);
+        else{
+            this.add(events);
+        }
 
         this.add(title);
         this.add(back);
-        this.add(eventScroll);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
