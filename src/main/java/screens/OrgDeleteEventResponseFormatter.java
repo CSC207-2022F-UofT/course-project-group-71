a2962b54f7583167356e6206ce89df0e76b2bc52
Baaadmtfka <zixuan.zeng@mail.utoronto.ca>
2022-11-12 19:54:22 -0500

    Fixed wrong message.

--- src/main/java/screens/OrgDeleteEventResponseFormatter.java
+++ src/main/java/screens/OrgDeleteEventResponseFormatter.java
@@ -6,7 +6,7 @@ import org_delete_event_use_case.OrgDeleteEventPresenter;
 public class OrgDeleteEventResponseFormatter implements OrgDeleteEventPresenter {
     @Override
     public OrgDeleteEventResponseModel prepareSuccessView(OrgDeleteEventResponseModel response) {
-        response.setMessage("Event" + response.getEventName() + "deleted");
+        response.setMessage("Event " + response.getEventName() + " deleted");
         return response;
     }
 }
