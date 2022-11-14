0fc77b037e2679f64b833078fd44ee4132de310e
RobinEatingWorm <gavinpu27@gmail.com>
2022-11-14 15:45:13 -0500

    Updated input classes

--- src/main/java/search_screens/EventSearchController.java
+++ src/main/java/search_screens/EventSearchController.java
@@ -4,4 +4,15 @@ import event_search_use_case.EventSearchInputBoundary;
 import event_search_use_case.EventSearchRequestModel;
 
 public class EventSearchController {
+
+    final EventSearchInputBoundary userInput;
+
+    public EventSearchController(EventSearchInputBoundary userInput) {
+        this.userInput = userInput;
+    }
+
+    public void eventSearch(String query) {
+        EventSearchRequestModel requestModel = new EventSearchRequestModel(query);
+        userInput.eventSearch(requestModel);
+    }
 }
