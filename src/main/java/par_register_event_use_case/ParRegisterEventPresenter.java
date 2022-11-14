69476dc1523f55963f17c9b879df57d280d68ced
Anita <hua0105anita@hotmail.com>
2022-11-14 15:24:48 -0500

    implemented presenter

--- src/main/java/par_register_event_use_case/ParRegisterEventPresenter.java
+++ src/main/java/par_register_event_use_case/ParRegisterEventPresenter.java
@@ -1,4 +1,5 @@
 package par_register_event_use_case;
 
 public interface ParRegisterEventPresenter {
+    ParRegisterEventResponseModel prepareSuccessView(ParRegisterEventResponseModel parRegisterEventResponseModel);
 }
--- src/main/java/par_register_event_use_case/ParRegisterEventResponseModel.java
+++ src/main/java/par_register_event_use_case/ParRegisterEventResponseModel.java
@@ -1,4 +1,5 @@
 package par_register_event_use_case;
 
 public class ParRegisterEventResponseModel {
+
 }
