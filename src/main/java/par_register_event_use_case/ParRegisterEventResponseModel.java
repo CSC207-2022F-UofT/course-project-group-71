6d4a732026fce16cc86211bee1e95f07e40ef929
Anita <hua0105anita@hotmail.com>
2022-11-15 15:28:23 -0500

    implemented ResponseModel

--- src/main/java/par_register_event_use_case/ParRegisterEventResponseModel.java
+++ src/main/java/par_register_event_use_case/ParRegisterEventResponseModel.java
@@ -1,6 +1,21 @@
 package par_register_event_use_case;
 
 public class ParRegisterEventResponseModel {
+    String eventName;
+    String message;
+
+    public ParRegisterEventResponseModel(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 
 
 }
