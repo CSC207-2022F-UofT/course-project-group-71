9443a0ec232b10de50dab6221f62e0f303f7166b
Anita <hua0105anita@hotmail.com>
2022-11-15 15:31:04 -0500

    Modified INteractor

--- src/main/java/par_register_event_use_case/ParRegisterEventInteractor.java
+++ src/main/java/par_register_event_use_case/ParRegisterEventInteractor.java
@@ -1,25 +1,21 @@
 package par_register_event_use_case;
 
-import database.OrgDsGateway;
 import database.ParDsGateway;
 import database.EventDsGateway;
 
 public class ParRegisterEventInteractor implements ParRegisterEventInputBoundary{
-    private ParRegisterEventPresenter parRegisterEventPresenter;
+    private final ParRegisterEventPresenter parRegisterEventPresenter;
     private final ParDsGateway parDsGateway;
-    private final EventDsGateway eventDsGateway;
-    private ParRegisterEventPresenter parRegisterEventPresenter;
 
     public ParRegisterEventInteractor(ParDsGateway parDsGateway, EventDsGateway eventDsGateway, ParRegisterEventPresenter parRegisterEventPresenter) {
         this.parDsGateway = parDsGateway;
-        this.eventDsGateway = eventDsGateway;
         this.parRegisterEventPresenter = parRegisterEventPresenter;
     }
 
     @Override
     public ParRegisterEventResponseModel register(ParRegisterEventRequestModel request_Model) {
         parDsGateway.registerEvent(request_Model.getPar_username(),request_Model.getEvent_name());
-        return parRegisterEventPresenter.prepareRegisterSuccess();
+        return parRegisterEventPresenter.prepareSuccessView();
     }
 
 
