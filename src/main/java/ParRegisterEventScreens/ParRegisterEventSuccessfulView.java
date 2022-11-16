a09fe7b84a2b81541666c636abb1ee4114ddd75f
Anita <hua0105anita@hotmail.com>
2022-11-16 12:47:34 -0500

    implemented successfulviewmodule

--- src/main/java/ParRegisterEventScreens/ParRegisterEventSuccessfulView.java
+++ src/main/java/ParRegisterEventScreens/ParRegisterEventSuccessfulView.java
@@ -1,5 +1,28 @@
 package ParRegisterEventScreens;
 
-public class ParRegisterEventSuccessfulView {
+import javax.swing.*;
 
+public class ParRegisterEventSuccessfulView {
+    private String message;
+
+    public static void main(String[] args) {
+        ParRegisterEventSuccessfulView a = new ParRegisterEventSuccessfulView("Congratulation! You registered this event successfully.");
+            a.GeneratePage();
+        }
+
+
+    public ParRegisterEventSuccessfulView(String message){
+            this.message = message;
+        }
+
+    public void GeneratePage(){
+        JFrame b = new JFrame("Success");
+        b.setBounds(500, 250, 400, 300);
+
+        JLabel label = new JLabel(this.message, SwingConstants.CENTER);
+        b.add(label);
+
+        b.setVisible(true);
+        b.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        }
 }
