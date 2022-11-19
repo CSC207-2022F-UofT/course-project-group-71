9ebbb23a0704e8f2bb5fb1e211779702dae3e79f
Baaadmtfka <zixuan.zeng@mail.utoronto.ca>
2022-11-19 09:25:30 -0500

    OrgDeleteEventPresenter doc. 2

--- src/main/java/org_delete_event_use_case/OrgDeleteEventPresenter.java
+++ src/main/java/org_delete_event_use_case/OrgDeleteEventPresenter.java
@@ -6,7 +6,7 @@ public interface OrgDeleteEventPresenter {
     /**A method used to show success view to the user
      *
      * @param orgDeleteEventResponseModel A response model containing information to show success view
-     * @return A response model showing failure view
+     * @return A response model showing success view
      */
     OrgDeleteEventResponseModel prepareSuccessView(OrgDeleteEventResponseModel orgDeleteEventResponseModel);
 }
