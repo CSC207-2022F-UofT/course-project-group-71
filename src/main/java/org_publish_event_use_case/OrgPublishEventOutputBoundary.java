5e9ed1d303cc7baf1b48a6067f1d8f67bbc73971
Baaadmtfka <zixuan.zeng@mail.utoronto.ca>
2022-11-19 15:53:28 -0500

    org_publish_event_use_case doc

--- src/main/java/org_publish_event_use_case/OrgPublishEventOutputBoundary.java
+++ src/main/java/org_publish_event_use_case/OrgPublishEventOutputBoundary.java
@@ -1,7 +1,14 @@
 package org_publish_event_use_case;
 
+/** Class used to prepare success view.
+ */
 public interface OrgPublishEventOutputBoundary {
 
+    /**A method used to show success view to the user
+     *
+     * @param orgPublishEventResponseModel A response model containing information to show success view
+     * @return A response model showing success view
+     */
     OrgPublishEventResponseModel prepareSuccessView(OrgPublishEventResponseModel orgPublishEventResponseModel);
 
 }
