07db7fd27c1a66976357514d504acffd2bfd0fdf
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-19 00:40:51 -0500

    finished test for user register

--- src/test/java/user_register_use_case/UserRegisterTest.java
+++ src/test/java/user_register_use_case/UserRegisterTest.java
@@ -1,19 +1,11 @@
-package database;
+package user_register_use_case;
+import database.OrgDsGateway;
+import database.OrgFileUser;
+import database.ParDsGateway;
+import database.ParFileUser;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import screens.*;
-import screens.org_home.OrgHomeResponseFormatter;
-import screens.par_home.ParHomeResponseFormatter;
-import user_login_use_case.*;
-import user_register_use_case.UserRegisterInputBoundary;
-import user_register_use_case.UserRegisterInteractor;
-import user_register_use_case.UserRegisterResponseModel;
-
-import javax.swing.*;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class UserRegisterTest {
@@ -84,10 +76,10 @@ public class UserRegisterTest {
     }
     @Test
     @Order(6)
-    void testPrepareSuccessView_Participant() {
+    void testPrepareFailView_ParticipantExists(){
         try {
             responseModel = userRegisterController.create("P", "",
-                    "a", "12345", "12345");
+                    "1", "12345", "12345");
             System.out.println(responseModel.getMessage());
             assert (false);
         } catch (Exception e) {
@@ -96,11 +88,11 @@ public class UserRegisterTest {
     }
     @Test
     @Order(7)
-    void testPrepareFailView_ParticipantExists(){
+    void testPrepareSuccessView_Participant(){
         try {
             responseModel = userRegisterController.create("P", "",
-                    "a", "12345", "12345");
-            assertEquals("a, you can login now!", responseModel.getMessage());
+                    "1", "12345", "12345");
+            assertEquals("1, you can login now!", responseModel.getMessage());
         } catch (Exception e) {
             System.out.println(e.getMessage());
             assert(false);
