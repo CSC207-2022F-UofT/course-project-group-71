1f767e7498564f5f447bf38b35df3bf9bd98f7e7
Baaadmtfka <zixuan.zeng@mail.utoronto.ca>
2022-11-19 09:28:12 -0500

    OrgDeleteEventRequestModel doc. 2

--- src/main/java/org_delete_event_use_case/OrgDeleteEventRequestModel.java
+++ src/main/java/org_delete_event_use_case/OrgDeleteEventRequestModel.java
@@ -1,11 +1,23 @@
 package org_delete_event_use_case;
 
+/** The request model of org_delete_event.
+ *  Contains eventName.
+ */
 public class OrgDeleteEventRequestModel {
 
     private String eventName;
+
+    /**A request model sent the interactor to create a user.
+     *
+     * @param eventName Name of event.
+     */
     public OrgDeleteEventRequestModel(String eventName) {
         this.eventName = eventName;
     }
+    /**A method to get the name of the event from the request model。
+     *
+     * @return eventName
+     */
     public String getEventName() {
         return eventName;
     }
