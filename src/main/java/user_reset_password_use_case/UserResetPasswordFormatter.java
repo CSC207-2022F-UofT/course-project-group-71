9fa1c291ee689137f9eb2e6639076b0fcb80d50d
songguangyu <songguangyu.song@mail.utoronto.ca>
2022-11-19 15:08:54 -0500

    reset password.document

--- src/main/java/user_reset_password_use_case/UserResetPasswordFormatter.java
+++ src/main/java/user_reset_password_use_case/UserResetPasswordFormatter.java
@@ -2,24 +2,13 @@ package user_reset_password_use_case;
 
 import screens.ShowMessageView;
 
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 public class UserResetPasswordFormatter implements UserResetPasswordPresenter{
-    public UserResetPasswordResponseModel prepareFailView(String message) {
+    /**This is a method used to prepare one of three view to user.
+     *
+     * @param message A String containing information about success or how to fail
+     * @return Response model to show some view
+     */
+    public UserResetPasswordResponseModel prepareView(String message) {
         throw new ShowMessageView(message);
-    }
-
-    public UserResetPasswordResponseModel prepareSuccessView(UserResetPasswordResponseModel responseModel) {
-        responseModel.setMessage(responseModel.getMessage());
-        StackWalker walker = StackWalker.getInstance();
-        Optional<String> prepareSuccessView = walker.walk(frames -> frames
-                .findFirst()
-                .map(StackWalker.StackFrame::getMethodName));
-        assertTrue(prepareSuccessView.isPresent());
-        assertEquals("prepareSuccessView", prepareSuccessView.get());
-        return responseModel;
     }
 }
--- src/main/java/user_reset_password_use_case/UserResetPasswordPresenter.java
+++ src/main/java/user_reset_password_use_case/UserResetPasswordPresenter.java
@@ -2,6 +2,12 @@ package user_reset_password_use_case;
 
 
 public interface UserResetPasswordPresenter {
+
+    /** A method used to show one of the three views to the user about whether reset successfully
+     *
+     * @param message A String containing information about success and the reason of fail.
+     * @return A response model showing if the resetname is success
+     */
     public UserResetPasswordResponseModel prepareFailView(String message);
 
     public UserResetPasswordResponseModel prepareSuccessView(UserResetPasswordResponseModel responseModel);
