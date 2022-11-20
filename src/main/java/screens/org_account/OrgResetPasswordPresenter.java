6c44d71a1d9f1ab6a03497aa07d0abb052bceb21
BenCheng2 <bencheng0609@gmail.com>
2022-11-19 23:19:00 -0500

    Version 2.1 Filling Some Page's JavaDoc

--- src/main/java/screens/org_account/OrgResetPasswordPresenter.java
+++ src/main/java/screens/org_account/OrgResetPasswordPresenter.java
@@ -7,12 +7,14 @@ import user_reset_password_use_case.UserResetPasswordResponseModel;
 public class OrgResetPasswordPresenter implements UserResetPasswordOutputBoundary {
     @Override
     public UserResetPasswordResponseModel prepareFailureView(String message) {
+        //Use ShowMessageView to show the message
         throw new ShowMessageView(message);
     }
 
     @Override
     public UserResetPasswordResponseModel prepareSuccessView(UserResetPasswordResponseModel responseModel) {
         responseModel.setMessage(responseModel.getMessage());
+        //Return the response model
         return responseModel;
     }
 
