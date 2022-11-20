410c21df8b0575e570afb606ed5cba0e765db4ce
songguangyu <songguangyu.song@mail.utoronto.ca>
2022-11-20 15:59:16 -0500

    UserLoginTest

--- src/test/java/user_login_use_case/UserLoginTest.java
+++ src/test/java/user_login_use_case/UserLoginTest.java
@@ -4,9 +4,14 @@ import database.OrgDsGateway;
 import database.OrgFileUser;
 import database.ParDsGateway;
 import database.ParFileUser;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import screens.UserLoginController;
 import screens.UserLoginPresenter;
 import screens.org_home.OrgHomePresenter;
 import screens.par_home.ParHomePresenter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UserLoginTest {
     ParDsGateway parDsGateway = new ParFileUser();
@@ -21,5 +26,31 @@ public class UserLoginTest {
 
     UserLoginInputBoundary interactor = new UserLoginInteractor(presenter, parDsGateway, par_presenter, orgDsGateway, org_presenter);
 
+    UserLoginController controller = new UserLoginController(interactor);
 
+    UserLoginResponseModel responseModel = null;
+
+    @Test
+    @Order(1)
+    void testPrepareFailView_ParticipantNotExist() {
+
+        try{
+            responseModel = UserLoginController.login("0","","kkk", "123123");
+            assert(false);
+        } catch (Exception e) {
+            assertEquals("Participant does not exist.", e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(2)
+    void testPrepareFailView_ParticipantPasswordNotMatch() {
+
+        try{
+            responseModel = UserLoginController.login("0","","aas", "123123");
+            assert(false);
+        } catch (Exception e) {
+            assertEquals("Password doesn't match.", e.getMessage());
+        }
+    }
 }
