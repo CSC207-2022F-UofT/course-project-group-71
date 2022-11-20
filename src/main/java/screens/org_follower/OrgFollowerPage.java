0a52176ed9bbd27dd26a72c6694f96f25b11792f
BenCheng2 <bencheng0609@gmail.com>
2022-11-19 23:27:07 -0500

    Version 2.1 Filling Some Page's JavaDoc

--- src/main/java/screens/org_follower/OrgFollowerPage.java
+++ src/main/java/screens/org_follower/OrgFollowerPage.java
@@ -22,77 +22,68 @@ public class OrgFollowerPage extends JFrame {
     public OrgFollowerPage(String orgUsername) throws SQLException, ClassNotFoundException {
         this.orgUsername = orgUsername;
 
+        //Set the parameter for the page
         this.setLayout(null);
-
         this.setSize(getConstantX(), getConstantY());
-
         this.setLocationRelativeTo(null);
 
+        //Set the title of the followers' page
         JLabel title = new JLabel(this.orgUsername + "'s Follower Page");
         title.setBounds(0, 0, getConstantX(), 50);
         title.setHorizontalAlignment(JLabel.CENTER);
 
+        //Add a button "Back", clicking it will come back to last page
         JButton back = new JButton("Back");
         back.addActionListener(new OrgFollowerActionListener(this));
         back.setBounds(0, 100, 150, 30);
 
-        JPanel events = new JPanel();
-        events.setBounds(150,100,getConstantX()-170,getConstantY()-150);
+        //Create a JPanel to store followers
+        JPanel followers = new JPanel();
+        followers.setBounds(150,100,getConstantX()-170,getConstantY()-150);
 
+        //Obtain OrgDsGateway and followers
         OrgDsGateway orgDsGateway = new OrgFileUser();
-
         ArrayList<String> Followers = orgDsGateway.getFollowers(orgUsername);
 
         int numberOfEvent = Followers.size();
 
+        //If there are events, there's going to be a scrolling bar with information in it
         if (numberOfEvent != 0) {
-
-            events.setLayout(new GridLayout(numberOfEvent, 0, 10, 10));
-
+            followers.setLayout(new GridLayout(numberOfEvent, 0, 10, 10));
             int x = 0;
             int y = 0;
 
+            //Add all followers to the followers panel
             for (String follower : Followers) {
-
+                //A button with the followers' name, can't be used to click
                 JButton organization = new JButton(follower);
                 organization.addActionListener(new OrgFollowerActionListener(this));
                 organization.setBounds(x, y, 250, 30);
                 organization.setVisible(true);
-
-//                JButton unFollow = new JButton("UnFollow");
-//                unFollow.setActionCommand(follower + "UnFollow");
-//                unFollow.addActionListener(new ParFollowedOrgActionListener(this));
-//                unFollow.setBounds(x + 250, y + 55, 100, 30);
-//                unFollow.setVisible(true);
-
-                events.add(organization);
-//                events.add(eventTime);
-//                events.add(eventLocation);
-//                events.add(notify);
-//                events.add(delete);
-//                events.add(unFollow);
+                followers.add(organization);
                 y += 100;
             }
-
-            JScrollPane followerscroll = new JScrollPane(events);
-            followerscroll.setBounds(150, 100, getConstantX() - 170, getConstantY() - 150);
-            followerscroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-            followerscroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-            followerscroll.setVisible(true);
-            this.add(followerscroll);
+            //Set the followerScroll as the container of followers JPanel
+            JScrollPane followerScroll = new JScrollPane(followers);
+            followerScroll.setBounds(150, 100, getConstantX() - 170, getConstantY() - 150);
+            followerScroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+            followerScroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+            followerScroll.setVisible(true);
+            //Add the followerScroll back to the screen
+            this.add(followerScroll);
         }
 
+        //Add the buttons to the screen
         this.add(title);
         this.add(back);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         this.setVisible(true);
     }
     /**The method returns organization's Username.
      * @return it will return a string which is organization's username.
      */
-    public String getOrgUsername() {
+    public String getOrgUsername(){
+        //Obtain the username of the organizer
         return orgUsername;
     }
 }
