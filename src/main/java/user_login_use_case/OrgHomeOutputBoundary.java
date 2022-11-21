2387199f112acdcc67ec3aa740feeea0460b05d1
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-21 14:48:55 -0500

    update 3rd

--- src/main/java/user_login_use_case/OrgHomeOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package user_login_use_case;
-
-public interface OrgHomeOutputBoundary {
-    UserLoginResponseModel prepareHomePageView(UserLoginResponseModel organization);
-}
--- src/main/java/user_reset_password_use_case/UserResetPasswordOutputBoundary.java
+++
@@ -1,7 +0,0 @@
-package user_reset_password_use_case;
-
-
-public interface UserResetPasswordOutputBoundary {
-    UserResetPasswordResponseModel prepareFailureView(String message);
-    UserResetPasswordResponseModel prepareSuccessView(UserResetPasswordResponseModel responseModel);
-}
