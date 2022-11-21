f3d47a3adcb7dc1971fa5ff26a3ddc55dc146834
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-20 22:32:40 -0500

    update

--- src/main/java/user_login_use_case/UserLoginInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package user_login_use_case;
-
-import java.sql.SQLException;
-
-public interface UserLoginInputBoundary {
-    UserLoginResponseModel login(UserLoginRequestModel requestModel) throws SQLException, ClassNotFoundException;
-}
