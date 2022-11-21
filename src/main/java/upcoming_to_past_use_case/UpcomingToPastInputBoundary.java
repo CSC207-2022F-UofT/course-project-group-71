d7f361f2f7d88c6f97e96af6c056b1ede0a77852
AndytheCreator223 <95309657+AndytheCreator223@users.noreply.github.com>
2022-11-21 05:59:56 -0500

    ready for pr

--- src/main/java/upcoming_to_past_use_case/UpcomingToPastInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package upcoming_to_past_use_case;
-
-import java.sql.SQLException;
-
-public interface UpcomingToPastInputBoundary {
-    UpcomingToPastResponseModel convertToPast(UpcomingToPastRequestModel requestModel) throws SQLException, ClassNotFoundException;
-}
