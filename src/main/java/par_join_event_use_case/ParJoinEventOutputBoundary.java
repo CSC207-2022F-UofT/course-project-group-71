9002a5bffb6b5c104aaa35d86e0ce489beb5d471
AndytheCreator223 <95309657+AndytheCreator223@users.noreply.github.com>
2022-11-21 05:54:29 -0500

    ready for pr

--- src/main/java/par_join_event_use_case/ParJoinEventOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package par_join_event_use_case;
-
-public interface ParJoinEventOutputBoundary {
-    ParJoinEventResponseModel prepareSuccessView(ParJoinEventResponseModel parJoinEventResponseModel);
-}
--- src/main/java/user_login_use_case/ParHomeOutputBoundary.java
+++
@@ -1,5 +0,0 @@
-package user_login_use_case;
-
-public interface ParHomeOutputBoundary {
-    UserLoginResponseModel prepareHomePageView(UserLoginResponseModel participant);
-}
