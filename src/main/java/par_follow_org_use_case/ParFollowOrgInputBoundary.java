06dc9dbf63df169207f4cfe4911fcd6d0180ac93
AndytheCreator223 <95309657+AndytheCreator223@users.noreply.github.com>
2022-11-21 05:51:05 -0500

    ready for pr

--- src/main/java/par_follow_org_use_case/ParFollowOrgInputBoundary.java
+++
@@ -1,7 +0,0 @@
-package par_follow_org_use_case;
-
-import java.sql.SQLException;
-
-public interface ParFollowOrgInputBoundary {
-    ParFollowOrgResponseModel follow(ParFollowOrgRequestModel request) throws SQLException, ClassNotFoundException;
-}
