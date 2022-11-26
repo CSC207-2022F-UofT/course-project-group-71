ff05e0038b26da89b602b3f9f46e13ca0cc2b129
AllysonCui <allyson.cui@mail.utoronto.ca>
2022-11-26 18:41:35 -0500

    update on tests

--- src/test/java/org_edit_event_use_case/OrgEditEventTest.java
+++ src/test/java/org_edit_event_use_case/OrgEditEventTest.java
@@ -24,6 +24,8 @@ public class OrgEditEventTest {
 
     OrgEditEventResponseModel responseModel = null;
 
+    /**Need to create an event called "Edit" in eventfile, can choose to leave other attributes null.
+     */
     @Test
     @Order(1)
     public void test_PrepareFailureView_entries_empty() {
@@ -37,6 +39,42 @@ public class OrgEditEventTest {
 
     @Test
     @Order(2)
+    void testPrepareFailViewDescriptionTooLong() {
+        try {
+            responseModel = controller.edit("Edit","An introduction to software design and development concepts, methods, and tools using a statically-typed object-oriented programming language such as Java. Topics from: version control, unit testing, refactoring, object-oriented design and development, design patterns, advanced IDE usage, regular expressions, and reflection. Representation of floating-point numbers and introduction to numerical computation.",
+                    "Zoom", "2024", "5", "2", "9", "9");
+            assert(false);
+        } catch (Exception e) {
+            assertEquals("Description should be no longer than 200 characters.", e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(3)
+    void testPrepareFailViewLocationTooLong() {
+        try {
+            responseModel = controller.edit("Edit", "SS", "Toronto ssssssssssssssssssssssssssssssssssssssssssssss ssssssssssssssssssssssssssssssssssssssssssssss",
+                    "2024", "5", "2", "9", "9");
+            assert(false);
+        } catch (Exception e) {
+            assertEquals("Location should be no longer than 50 characters.", e.getMessage());
+        }
+    }
+
+    @Test
+    @Order(4)
+    public void test_PrepareFailureView_not_integer() {
+        try {
+            responseModel = controller.edit("Edit", "HH", "Zoom", "aja", "4", "2", "9", "9");
+            assert (false);
+        } catch (Exception e) {
+            assertEquals("Time entry/ies is/are not integer.", e.getMessage());
+
+        }
+    }
+
+    @Test
+    @Order(5)
     public void test_PrepareFailureView_wrong_year() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "200", "4", "2", "9", "9");
@@ -47,7 +85,7 @@ public class OrgEditEventTest {
     }
 
     @Test
-    @Order(3)
+    @Order(6)
     public void test_PrepareFailureView_wrong_month() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "2024", "13", "2", "9", "9");
@@ -58,7 +96,7 @@ public class OrgEditEventTest {
     }
 
     @Test
-    @Order(4)
+    @Order(7)
     public void test_PrepareFailureView_wrong_day() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "2024", "4", "40", "9", "9");
@@ -69,18 +107,18 @@ public class OrgEditEventTest {
     }
 
     @Test
-    @Order(5)
+    @Order(8)
     public void test_PrepareFailureView_wrong_hour() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "2024", "4", "2", "30", "9");
             assert (false);
         } catch (Exception e) {
-            assertEquals("Day is not within 0 to 24.", e.getMessage());
+            assertEquals("Hour is not within 0 to 24.", e.getMessage());
         }
     }
 
     @Test
-    @Order(6)
+    @Order(9)
     public void test_PrepareFailureView_wrong_minite() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "2024", "4", "2", "9", "70");
@@ -93,7 +131,7 @@ public class OrgEditEventTest {
     }
 
     @Test
-    @Order(7)
+    @Order(10)
     public void test_PrepareFailureView_future_time() {
         try {
             responseModel = controller.edit("Edit", "HH", "Zoom", "2004", "4", "2", "9", "9");
@@ -104,24 +142,14 @@ public class OrgEditEventTest {
         }
     }
 
-    @Test
-    @Order(7)
-    public void test_PrepareFailureView_not_integer() {
-        try {
-            responseModel = controller.edit("Edit", "HH", "Zoom", "aja", "4", "2", "9", "9");
-            assert (false);
-        } catch (Exception e) {
-            assertEquals("Time entry/ies is/are not integer.", e.getMessage());
 
-        }
-    }
 
     @Test
-    @Order(8)
+    @Order(11)
     public void test_PrepareSuccessView()  {
         try{
             responseModel = controller.edit("Edit", "SS", "Zoom", "2024", "5", "2", "9", "9");
-            assertEquals("Event Edit is successfully edited!", responseModel.getTitle());
+            assertEquals("Event Edit is successfully edited!", responseModel.getMessage());
             controller.edit("Edit", "GG", "Zoom", "2024", "5", "2", "9", "9");
         } catch (Exception e) {
             assert(false);
