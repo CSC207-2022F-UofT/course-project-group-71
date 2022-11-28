418a0bbd4ff337a7d486ead9c53bc1f98059e796
Anita <hua0105anita@hotmail.com>
2022-11-28 16:53:49 -0500

    Modifited Interactor

--- src/main/java/Par_Join_Event_Use_Case/ParJoinEventInteractor.java
+++ src/main/java/Par_Join_Event_Use_Case/ParJoinEventInteractor.java
@@ -8,7 +8,23 @@ public class ParJoinEventInteractor implements ParJoinEventInputBoundary {
     ParJoinEventOutputBoundary presenter;
     ParDsGateway parDsGateway;
 
+    /**This is the construct method of ParJoinEventInteractor.
+     * It takes DsGateways and Presenter as input to store as instances.
+     *
+     * @param presenter The presenter used to show successful view when registered successes.
+     * @param parDsGateway The database gateway of the participants.
+     */
+    public ParJoinEventInteractor(ParDsGateway parDsGateway, ParJoinEventOutputBoundary presenter) {
+        this.parDsGateway = parDsGateway;
+        this.presenter = presenter;
+    }
 
+    /**Use the provided method from parDsGateway to make a participant join an upcoming event.
+     * This method is called when the participant is searching for events.
+     *
+     * @param requestModel The request model sent to this interactor.
+     * @return A responseModel representing the user registered for the event successfully by the presenter.
+     */
     @Override
     public ParJoinEventResponseModel join(ParJoinEventRequestModel requestModel) throws SQLException, ClassNotFoundException {
         parDsGateway.registerEvent(requestModel.getParUsername(), requestModel.getEventTitle());
