e4961a29ea8eb478e9f2dbcf109c70018134bb78
RobinEatingWorm <gavinpu27@gmail.com>
2022-11-27 20:47:45 -0500

    added comments

--- src/main/java/use_cases/extract_information_use_case/ExtractInfoInputBoundary.java
+++ src/main/java/use_cases/extract_information_use_case/ExtractInfoInputBoundary.java
@@ -2,11 +2,34 @@ package use_cases.extract_information_use_case;
 
 public interface ExtractInfoInputBoundary {
 
+    /**A method for getting event information that is not the event time
+     *
+     * @param requestModel A request model containing the event title
+     *                     and which information to extract
+     * @return A response model containing the desired information
+     */
     ExtractInfoResponseModel<String> extractEventInfo(ExtractInfoRequestModel requestModel);
 
+    /**A method for getting organization inforamtion
+     *
+     * @param requestModel A request model containing the organization username
+     *                     and which information to extract
+     * @return A response model containing the desired information
+     */
     ExtractInfoResponseModel<String> extractOrgInfo(ExtractInfoRequestModel requestModel);
 
+    /**A method for getting participant information
+     *
+     * @param requestModel A request model containing the participant username
+     *                     and which information to extract
+     * @return A response model containing the desired information
+     */
     ExtractInfoResponseModel<String> extractParInfo(ExtractInfoRequestModel requestModel);
 
+    /**A method for getting event time information
+     *
+     * @param requestModel A request model containing the event title
+     * @return A response model containing the event time
+     */
     ExtractInfoResponseModel<Integer> extractTimeInfo(ExtractInfoRequestModel requestModel);
 }
