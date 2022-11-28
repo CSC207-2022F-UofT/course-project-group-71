97c5ea7e976d96a355988d400f5cd11fed75d9de
BenCheng2 <bencheng0609@gmail.com>
2022-11-28 17:54:37 -0500

    Version 2.2 Readme writing

--- src/main/java/use_cases/user_register_use_case/UserRegisterResponseModel.java
+++ src/main/java/use_cases/user_register_use_case/UserRegisterResponseModel.java
@@ -1,1 +1,29 @@
 package use_cases.user_register_use_case;
+
+/** The response model sent back to the page.
+ *  Containing the username and the message.
+ */
+public class UserRegisterResponseModel {
+    String username;
+    String message;
+
+    public UserRegisterResponseModel(String username){
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
