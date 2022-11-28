a0e5c5e326f89182255e56cc64dbe609bff685b0
BenCheng2 <bencheng0609@gmail.com>
2022-11-28 17:42:04 -0500

    Version 2.2 Readme writing

--- src/main/java/Par_Join_Event_Use_Case/ParJoinEventOutputBoundary.java
+++
@@ -1,6 +0,0 @@
-package Par_Join_Event_Use_Case;
-
-public interface ParJoinEventOutputBoundary {
-    ParJoinEventResponseModel prepareSuccessView(ParJoinEventResponseModel parJoinEventResponseModel);
-    //since the regiser of the event will not fail, we only have to prepare the success view.
-}
