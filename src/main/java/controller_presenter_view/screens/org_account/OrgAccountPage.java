c15c929b738882c926a15405030f72ce996cec3e
Anita <hua0105anita@hotmail.com>
2022-11-28 23:08:26 -0500

    modified orgaccountpage to reduce the length of the function

--- src/main/java/controller_presenter_view/screens/org_account/OrgAccountPage.java
+++ src/main/java/controller_presenter_view/screens/org_account/OrgAccountPage.java
@@ -30,6 +30,21 @@ public class OrgAccountPage extends JFrame {
         output.setBounds (x,y, width, height);
         return output;
     }
+    /**
+     * This is the function to generate a JLabel
+     * @param text the text we want to show
+     * @param x the integer x for set bounds
+     * @param y the integer y for set bounds
+     * @param width the integer representing the width for set bounds
+     * @param height the integer representing the height for set bounds
+     * @return a text panel
+     */
+    public JLabel create_Jlable(String text, int x, int y, int width, int height){
+        JLabel output = new JLabel(text);
+        output.setBounds(x, y, width, height);
+        output.setHorizontalAlignment(JLabel.CENTER);
+        return output;
+    }
     /**The method generate an Organization Account page and allowed the organization to reset password.
      * It contains a title with organization's username.
      * It allows user to input old and new password to reset password with a button called "Reset Password".
@@ -46,32 +61,6 @@ public class OrgAccountPage extends JFrame {
         this.setSize(getConstantX(), getConstantY());
         this.setLocationRelativeTo(null);
 
-        //Set the title
-        JLabel title = new JLabel(this.orgUsername + "'s Account Page");
-        title.setBounds(0, 0, getConstantX(), 50);
-        title.setHorizontalAlignment(JLabel.CENTER);
-
-        //Set the username label
-        JLabel username = new JLabel("Username                      " + this.orgUsername + "                ");
-        username.setBounds(150, 100, 500,30);
-        username.setHorizontalAlignment(JLabel.CENTER);
-
-        //A Text panel to input the old password
-//        LabelTextPanel oldPasswordInfo = new LabelTextPanel(
-//                new JLabel("Old Password"), oldPassword);
-//        oldPasswordInfo.setBounds (150,130, 500, 50);
-        // create_text_panel("Old Password", oldPassword, 150, 130, 500, 50);
-
-        //A Test panel to input the new pasword
-//        LabelTextPanel newPasswordInfo = new LabelTextPanel(
-//                new JLabel("New Password"), newPassword);
-//        newPasswordInfo.setBounds (150,180, 500, 50);
-
-        //A Test panel to re-input the new password
-//        LabelTextPanel retypeNewPasswordInfo = new LabelTextPanel(
-//                new JLabel("Retype New Password"), retypeNewPassword);
-//        retypeNewPasswordInfo.setBounds (150,230, 500, 50);
-
         //A button to reset password
         JButton resetPassword = new JButton("Reset Password");
         resetPassword.addActionListener(new OrgAccountActionListener(this));
@@ -86,10 +75,11 @@ public class OrgAccountPage extends JFrame {
         back.addActionListener(new OrgAccountActionListener(this));
         back.setBounds(0, 100, 150, 30);
 
-        //Add the buttons to the page
-        this.add(title);
+        //Add the buttons and text panels to the page
+        this.add(create_Jlable(this.orgUsername + "'s Account Page", 0, 0, getConstantX(), 50););
         this.add(back);
-        this.add(username);
+        this.add(        create_Jlable("Username                      " + this.orgUsername + "                "
+                , 150, 100, 500,30));
         this.add(create_text_panel("Old Password", oldPassword, 150, 130, 500, 50));
         this.add(create_text_panel("New Password",  newPassword, 150, 180, 500, 50));
         this.add(create_text_panel("Retype New Password",  newPassword, 150, 230, 500, 50));
