029090f176692e3e910cf95a933e6f4dae7025d1
Anita <hua0105anita@hotmail.com>
2022-11-29 22:40:48 -0500

    extract the repeated code of OrgHomePage into a function, which reduced the length of the code and make the codes simpler. I didn't delete all other codes but commented them, so we can still change it back if it is not working.

--- src/main/java/controller_presenter_view/screens/org_home/OrgHomePage.java
+++ src/main/java/controller_presenter_view/screens/org_home/OrgHomePage.java
@@ -10,6 +10,21 @@ public class OrgHomePage extends JFrame {
 
     private final String orgUsername;
 
+    /**
+     * This function returns a JButton with the input as set bounds
+     * @param text string of the button showing
+     * @param x the integer x for set bounds
+     * @param y the integer y for set bounds
+     * @param width the integer representing the width for set bounds
+     * @param height the integer representing the height for set bounds
+     * @return return the JButton
+     */
+    public JButton create_jbutton(String text, int x, int y, int width, int height){
+        JButton output = new JButton(text);
+        output.addActionListener(new OrgHomeActionListener(this));
+        output.setBounds (x, y, width, height);
+        return output;
+    }
     /**The method generate a page of organization's homepage.
      * It contains six buttons at left vertically.
      * "Account" button will jump to the account page which can let the organization to changes password.
@@ -38,37 +53,37 @@ public class OrgHomePage extends JFrame {
         title.setBounds (0,0, getConstantX(), 50);
         title.setHorizontalAlignment(JLabel.CENTER);
 
-        JButton account = new JButton("Account");
-        account.addActionListener(new OrgHomeActionListener(this));
-        account.setBounds (0,150, 150, 30);
-
-        JButton unpublishedEvent = new JButton("Unpublished Event");
-        unpublishedEvent.addActionListener(new OrgHomeActionListener(this));
-        unpublishedEvent.setBounds (0,210, 150, 30);
-
-        JButton upcomingEvent = new JButton("Upcoming Event");
-        upcomingEvent.addActionListener(new OrgHomeActionListener(this));
-        upcomingEvent.setBounds (0,270, 150, 30);
-
-        JButton pastEvent = new JButton("Past Event");
-        pastEvent.addActionListener(new OrgHomeActionListener(this));
-        pastEvent.setBounds (0,330, 150, 30);
-
-        JButton follower = new JButton("Follower");
-        follower.addActionListener(new OrgHomeActionListener(this));
-        follower.setBounds (0,390, 150, 30);
-
-        JButton logOut = new JButton("Log Out");
-        logOut.addActionListener(new OrgHomeActionListener(this));
-        logOut.setBounds (0,510, 150, 30);
+//        JButton account = new JButton("Account");
+//        account.addActionListener(new OrgHomeActionListener(this));
+//        account.setBounds (0,150, 150, 30);
+//
+//        JButton unpublishedEvent = new JButton("Unpublished Event");
+//        unpublishedEvent.addActionListener(new OrgHomeActionListener(this));
+//        unpublishedEvent.setBounds (0,210, 150, 30);
+//
+//        JButton upcomingEvent = new JButton("Upcoming Event");
+//        upcomingEvent.addActionListener(new OrgHomeActionListener(this));
+//        upcomingEvent.setBounds (0,270, 150, 30);
+//
+//        JButton pastEvent = new JButton("Past Event");
+//        pastEvent.addActionListener(new OrgHomeActionListener(this));
+//        pastEvent.setBounds (0,330, 150, 30);
+//
+//        JButton follower = new JButton("Follower");
+//        follower.addActionListener(new OrgHomeActionListener(this));
+//        follower.setBounds (0,390, 150, 30);
+//
+//        JButton logOut = new JButton("Log Out");
+//        logOut.addActionListener(new OrgHomeActionListener(this));
+//        logOut.setBounds (0,510, 150, 30);
 
         this.add(title);
-        this.add(account);
-        this.add(unpublishedEvent);
-        this.add(upcomingEvent);
-        this.add(pastEvent);
-        this.add(follower);
-        this.add(logOut);
+        this.add(create_jbutton("Account", 0,150, 150, 30));
+        this.add(create_jbutton("Unpublished Event", 0,210, 150, 30));
+        this.add(create_jbutton("Upcoming Event", 0,270, 150, 30));
+        this.add(create_jbutton("Past Event", 0,330, 150, 30));
+        this.add(create_jbutton("Follower", 0,390, 150, 30));
+        this.add(create_jbutton("Log Out", 0,510, 150, 30));
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -81,3 +96,4 @@ public class OrgHomePage extends JFrame {
     public String getOrgUsername() { return this.orgUsername;}
 
 }
+
