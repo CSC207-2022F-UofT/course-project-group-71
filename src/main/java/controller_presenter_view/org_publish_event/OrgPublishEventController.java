6ed24a9e1a2541bedb690b2087726e253d257037
Anita <hua0105anita@hotmail.com>
2022-11-29 20:41:38 -0500

    Added organization public event use case, including all implementations following clean architecture and a tests tested on OrgPublishEvent.

---
+++ src/main/java/controller_presenter_view/org_publish_event/OrgPublishEventController.java
@@ -0,0 +1,23 @@
+package controller_presenter_view.org_publish_event;
+
+import use_cases.org_publish_event_use_case.OrgPublishEventInputBoundary;
+import use_cases.org_publish_event_use_case.OrgPublishEventRequestModel;
+import use_cases.org_publish_event_use_case.OrgPublishEventResponseModel;
+
+import java.sql.SQLException;
+
+public class OrgPublishEventController {
+    final OrgPublishEventInputBoundary userInput;
+    //
+    public OrgPublishEventController(OrgPublishEventInputBoundary accountGateway) {
+        //Set the input interactor as instance
+        this.userInput = accountGateway;
+    }
+
+    public OrgPublishEventResponseModel publish(String eventTitle, String orgUsername) throws SQLException, ClassNotFoundException {
+        //Generate a new request model by the given information
+        OrgPublishEventRequestModel requestModel = new OrgPublishEventRequestModel(eventTitle, orgUsername);
+        return userInput.publish(requestModel);
+    }
+
+}
